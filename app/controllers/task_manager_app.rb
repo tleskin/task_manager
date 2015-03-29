a98710fcad2bde99b0f9c54d144b138d97ca8fe8
Thomas Leskin <tleskin@gmail.com>
2015-03-29 17:30:00 -0600

    first commit plus implemented Task Index Route

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,16 @@
+class TaskManagerApp < Sinatra::Base
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = ["task1", "task2", "task3"]
+    erb :index
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+end
